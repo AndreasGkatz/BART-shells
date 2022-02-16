4829ae3a47d9677d9e489874695e0e5347cc98af
andreas <andreas.gkatz@gmail.com>
2022-02-16 14:33:50 +0100

    my first pessimistic commit

---
+++ example_shell_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is an example shell script
+# <--- this symbol here identifies a comment
+# Before executing the first time, you need to run
+# chmod +x example_shell_script.sh
+# afterwards you can execute it with
+# ./example_shell_script.sh
+
+echo "Hello World and Goodbye Cruel World!"
